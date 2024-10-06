c2f8efa327e4d432734c5f11966b18c6b7ecbaed
Juan Pablo <144071391+jpz555@users.noreply.github.com>
2024-10-06 01:35:19 -0500

    Update app_1.js

--- app_1.js
+++ app_1.js
@@ -58,7 +58,7 @@ function generarNumeroSecreto() {
 
 function condicionesIniciales() {
     asignarTextoElemento('h1', "Juego del numero secreto");
-    asignarTextoElemento('p', `Inidica un numero del 1 al ${numeroMaximo}`);
+    asignarTextoElemento('p', `Indica un numero del 1 al ${numeroMaximo}`);
     numeroSecreto = generarNumeroSecreto();
     intentos = 1;
 }
